57e87c8a0b6d6f1872a373c4aed0d960358b0532
curso07 <alejandro.cald@gmail.com>
2024-12-17 13:58:41 +0100

    Corte por salida de aula

--- front/src/scripts/index.js
+++ front/src/scripts/index.js
@@ -1,4 +1,7 @@
 console.log("Empieza la página...")
+
+let articulos = [];
+
 $.get("http://localhost:1234/api/productos")
 
 $(document).ready(function () {
@@ -20,18 +23,25 @@ $(document).ready(function () {
                     data.forEach(x => {
                         console.log("Processing item:", x);
                         let $linea = $('<tr>');
+
+                        articulos.push({id: x.id, nombre: x.nombre});
                         $linea.append($('<td class="renglon mt-3 md-3" style=display:none>').text(x.id));
                         $linea.append($('<td class="renglon mt-3 md-3">').text(x.nombre));
                         $linea.append($('<td class="renglon mt-3md-3">').text(x.descripcion));
                         $linea.append($('<td class="renglon mt-3md-3">').text(x.cantidad));
-                        $linea.append($('<td class="renglon mt-3md-3">').text(x.precio));;
+                        $linea.append($('<td class="renglon mt-3md-3">').text(x.precio));
+                        $linea.append($('<button id="boton_detalle" class="btn btn-info btn-lg botonera">Detalle</button>'));
+                        $linea.append($('<button id="boton_venta" class="btn btn-success btn-lg botonera">Venta</button>'));
+                        $linea.append($('<button id="altas" class="btn btn-warning btn-lg botonera"> Alta </button>'));
+                        $linea.append($('<button id="baja" class="btn btn-danger btn-lg botonera"> Baja </button>'));
                         $padre.append($linea);
-                        
+
                     });
-                    
+
                     $maestro.show();
                     $padre.show();
                     console.log("Refrescaso de tabla ejecutado");
+                    console.log(articulos);
 
                 }).fail(function () {
 
@@ -42,14 +52,65 @@ $(document).ready(function () {
 
         window.refrescarListado = refrescarListado;
 
-       
+
     };
 
-    $("#boton_lista").on("click", function () {
 
-        refrescarListado();
+    function detalleVenta() {
+
+        
+        articulos.forEach(x => {
+            console.log(x.id);
+            let $select = $("#productos_venta");
+            let $option = $("<option>");
+            $option.append($('<select>').text(x.nombre));
+            $select.append($option);
+
+        });
+
+        $padre_venta = $("#venta");
+        $padre_venta.show();
+
+    }
+
+    window.detalleVenta = detalleVenta
+
+    $("#boton_venta").on("click", function () {
+
+        detalleVenta();
 
     });
 
+    refrescarListado();
 
+
+    $('#listado').on("click", "#boton_baja", function (event) {
+
+        event.preventDefault() // Esto o si no, al menos en Brave, cuelga por razòn no informada en inspector y debugguer. 
+
+        let $row = $(this).closest('tr');
+        let solId = $row.find('td').eq(0).text();
+
+        $.ajax({url: 'http://localhost:1234/api/productos' + solId,
+            method: "PUT",
+            contentType: "application/json",
+            success: function(result){
+                
+                console.log('Respuesta: ' + result)
+                let $padre = $('<respuesta></respuesta')
+                let $parrafo = $('<p class="Roboto"></p>').text("Respuesta del Servidor tras DELETE: " + JSON.stringify(result) + ". Para solID: " + solId);
+                $padre.append($parrafo);
+    
+                $('body').append($padre);
+            },
+            error: function(xhr, status, error) { 
+                let $parrafo0 = $('<p class="Roboto></p>').text("Respuesta del Servidor");
+                let $parrafo = $('<p class="Roboto></p>').text('Error: ' + error);
+            } 
+        });
+
+        refrescarListado();
+
+
+    });
 });
